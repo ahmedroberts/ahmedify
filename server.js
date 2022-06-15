083aa404eb489483f9f6836215485c4a564f977a
Ahmed Roberts <ahmedroberts@gmail.com>
2022-06-15 15:48:08 -0400

    setup connection to local mongoDB, uses dotenv as well

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // 7.  Check if this is development or production
+// Make Sure .env is top-level file
 if (process.env.NODE_ENV !== 'production'){
-  require('dotenv')
+  require('dotenv').config()
 }
 
 // 1. Setting up the Express Server as an App
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+
+console.log(process.env.DATABASE_URL);
 
 // 4. Require our routers
 const indexRouter = require('./routes/index')
@@ -18,7 +21,13 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('connection', () => console.log('Connected to Mongoose'))
 
 // 5. Use Routers inside the app just like Controllers
 app.use('/', indexRouter)
