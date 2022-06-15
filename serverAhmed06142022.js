d17da06fd6d8d426908f600d19c71a15d97845ed
Ahmed Roberts <ahmedroberts@gmail.com>
2022-06-15 15:05:32 -0400

    Working dynamic layouts

--- serverAhmed06142022.js
+++ serverAhmed06142022.js
@@ -18,7 +18,23 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+// 6. Setup Database in the app MongoDB Atlas for me
+// Had to do my own research
 
+// 8. Connected to mongoose Atlas using online documentation
+//Import the mongoose module
+const mongoose = require('mongoose');
+
+//Set up default mongoose connection
+const mongoDB = 'mongodb+srv://adminahmed:z$BlueMarvel1313@cluster0.sf3ee.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+const db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('Connected to Mongoose'))
 
 // 5. Use Routers inside the app just like Controllers
 app.use('/', indexRouter)
